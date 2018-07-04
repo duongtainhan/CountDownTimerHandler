66d31c29b7cb84bb7d5bb11897248d18f383fe67
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-04 23:46:06 +0700

    Random pictures

--- app/src/main/java/com/example/duongtainhan555/countdowntimerhandler/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/countdowntimerhandler/MainActivity.java
@@ -2,24 +2,33 @@ package com.example.duongtainhan555.countdowntimerhandler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgvPicture;
+    ImageView imgPicture;
     Button btnRandom;
-    int r[] = {};
+    int[] r = {R.drawable.ic_facebook, R.drawable.ic_ex, R.drawable.ic_setting, R.drawable.ic_speaker,R.drawable.ic_twitter, R.drawable.ic_vn};
+    Random rd;
+    int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgvPicture = findViewById(R.id.imgvPicture);
+        imgPicture = findViewById(R.id.imgPicture);
         btnRandom  = findViewById(R.id.btnRandom);
-
-        Random rd = new Random();
+        rd = new Random();
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                index = rd.nextInt(r.length);
+                imgPicture.setImageResource(r[index]);
+            }
+        });
 
     }
 }

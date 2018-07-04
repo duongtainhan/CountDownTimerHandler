cb8760930c0db54bc14cb343e148e27ff989f0fb
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-04 23:08:59 +0700

    Ánh xạ

--- app/src/main/java/com/example/duongtainhan555/countdowntimerhandler/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/countdowntimerhandler/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.duongtainhan555.countdowntimerhandler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgvPicture;
+    Button btnRandom;
+    int r[] = {};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgvPicture = findViewById(R.id.imgvPicture);
+        btnRandom  = findViewById(R.id.btnRandom);
+
+        Random rd = new Random();
+
     }
 }
